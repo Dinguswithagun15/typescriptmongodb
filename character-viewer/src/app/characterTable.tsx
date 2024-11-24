bba4b8e109355addddf29a485a4c6c392b33828d
Adam Caldwell <adamcaldwell15@gmail.com>
2024-11-24 15:56:10 +0000

    trying to retrieve data from mongodb db

---
+++ character-viewer/src/app/characterTable.tsx
@@ -0,0 +1,12 @@
+import { Collection } from "mongoose";
+
+interface Character {
+    "alias": string;
+    "name": string;
+}
+
+export default function CharacterTable(props) {
+    return (
+        <p>test</p>
+    )
+}
--- character-viewer/src/app/page.tsx
+++ character-viewer/src/app/page.tsx
@@ -1,8 +1,20 @@
 import React from "react";
-const Page = () => {
-  return (
-    <p>Test</p>
-  )
+import CharacterTable from "./characterTable";
+import {Collection, MongoClient} from "mongodb";
+
+interface Character {
+  "alias": string;
+  "name": string;
 }
 
-export default Page;
+export default async function Page() {
+  const uri = "mongodb+srv://adamcaldwell15:PinkZebra21*@charactercluster.lra1z.mongodb.net/?retryWrites=true&w=majority&appName=CharacterCluster"; 
+  const client = new MongoClient(uri);
+  await client.connect();
+  const database = client.db("test");
+  const characters: Collection<Character> = database.collection<Character>("characters");
+  await client.close();
+  return (
+    <CharacterTable characters={characters}/>
+  )
+}
